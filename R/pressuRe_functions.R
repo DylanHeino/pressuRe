ab30b54836a6019cb9f28b1f6f3d463f3443ab2d
DylanHeino <146702003+DylanHeino@users.noreply.github.com>
2024-10-15 14:41:13 -0700

    changed max to mean and max to meanmax in plot_pressure

--- R/pressuRe_functions.R
+++ R/pressuRe_functions.R
@@ -1138,7 +1138,7 @@ footprint <- function(pressure_data, variable = "max", frame = NULL,
 #' @importFrom scales manual_pal
 #' @export
 
-plot_pressure <- function(pressure_data, variable = "max", smooth = FALSE, frame,
+plot_pressure <- function(pressure_data, variable = "mean", smooth = FALSE, frame,
                           step_n = "meanmax", plot_COP = FALSE, plot_outline = FALSE,
                           plot_colors = "default", break_values, break_colors,
                           sensor_outline = TRUE, plot = TRUE, legend = TRUE) {
